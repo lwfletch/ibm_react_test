f19c8aabcd62d14603998a688116650528952c52
Trey Fletcher <lefletch@cisco.com>
2017-07-11 22:29:32 -0400

    got app laid out, need to add functionality and database

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Form, ControlLabel, FormControl, FormGroup, Button } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <div [style.margin-top.px]="10" className="row">
+        <div className="row">
           <div className="col-md-6">
             <h3>Add new person</h3>
-            <form className="form-inline">
-              <div className="form-group">
-                <label for="first_name">First Name</label>
-                <input type="text" className="form-control" id="first_name" #first_name>
-              </div>
-              <div className="form-group">
-                <label for="last_name">Last Name</label>
-                <input type="text" className="form-control" id="last_name" #last_name>
-              </div>
-              <div className="form-group">
-                <label for="address">Address</label>
-                <input type="address" className="form-control" id="address" #address>
-              </div>
-              <div className="form-group">
-                <label for="company">Company</label>
-                <input type="text" className="form-control" id="company" #company>
-              </div>
-              <br>
-              <button type="button" (click)="addNew(first_name.value, last_name.value, address.value, company.value)" class="btn btn-primary">Submit</button>
-            </form>
+            <Form>
+              <FormGroup controlId="formInlineFirstName">
+                <ControlLabel>First Name</ControlLabel>
+                {' '}
+                <FormControl type="text"/>
+              </FormGroup>
+                {' '}
+              <FormGroup controlId="formInlineLastName">
+                <ControlLabel>Last Name</ControlLabel>
+                {' '}
+                <FormControl type="text"/>
+              </FormGroup>
+                {' '}
+              <FormGroup controlId="formInlineAddress">
+                <ControlLabel>Address</ControlLabel>
+                {' '}
+                <FormControl type="address"/>
+              </FormGroup>
+              {' '}
+              <FormGroup controlId="formInlineCompany">
+                <ControlLabel>Company</ControlLabel>
+                {' '}
+                <FormControl type="text"/>
+              </FormGroup>
+              {' '}
+              <Button bsStyle="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
           </div>
           <div className="col-md-6">
-            <div [style.margin-top.px]="10" className="row">
+            <div className="row">
               <h3>Results</h3>
-              <!-- Bootstrap Card -->
-              <div [style.margin-right.px]="10" className="row card card-block">
-                <p>First Name: </p>
-                <p>Last Name: </p> 
-                <p>Address: </p> 
-                <p>Company: </p>
-              </div>
             </div>
           </div>
         </div>
